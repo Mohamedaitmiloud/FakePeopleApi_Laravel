1ebe712b0e01c54f519b896c310d91e4f391b531
Mohamed Aitmiloud <mohammed.aitmiloud@gmail.com>
2018-12-06 00:50:12 +0100

    setting api routs

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Http\Request;
 
+
+
+
+
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+  Public API Routes
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function() {
+
+    Route::resource('client', 'ClientController')->except('create','edit','update');
+    
+    Route::put('/client/{id}',[
+        'uses'=>'ClientController@update',
+        'as'=>'client.update'
+    ]);
+    
 });
+
